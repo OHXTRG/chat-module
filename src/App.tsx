4599cddca70fa9041748365ed4fa582c9055ef9d
OHXTRG <119294244+OHXTRG@users.noreply.github.com>
2025-11-18 23:06:15 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-yellow-900 text-center font-extrabold ">
+        Tailwind Vite
+      </h1>
+    </>
+  );
+}
+
+export default App;
